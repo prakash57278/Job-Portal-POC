ee2a3b0d958fb6aaa00d9e553d19410846671ad2
Prakash Singh <prakashtawat2@gmail.com>
2025-05-29 23:16:23 +0530

    Initial commit

---
+++ src/main/java/com/jobPortalPoc/JobPortalPocApplication.java
@@ -0,0 +1,13 @@
+package com.jobPortalPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobPortalPoc/JobPortalPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobPortalPoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
